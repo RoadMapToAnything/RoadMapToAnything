b70d0509da58ee30560db8c0babb654c936facc5
Sean Reimer <sreimer15@gmail.com>
2016-01-21 12:35:34 -0800

    Sets up nested views

--- client/app.js
+++ client/app.js
@@ -1,17 +1,38 @@
+
 angular.module('app', [
   'ui.router',
   'app.main',
   'app.auth'
+  
   ])
-
+// Placeholder header and footer
 .config(function($stateProvider, $urlRouterProvider, $httpProvider) {
   $urlRouterProvider.otherwise('/');
   $stateProvider
     .state('main', {
       url: '/main',
       templateUrl: 'main/main.html',
+      views: {
+        "footer": {
+          template: "main.footer"
+        },
+        "header": {
+          template: "main.header"
+        }
+      }
       controller: "MainController"
     })
+      // .state('main.footer', {
+      //   url: '/footer',
+      //   templateUrl: "main/main.footer.html",
+      //   controller: "MainController"
+      // })
+      // .state('main.header', {
+      //   url: '/header',
+      //   templateUrl: "main/main.footer.html",
+      //   controller: "MainController"
+      // })
+
     .state('signup', {
       url: "/signup",
       templateUrl: "app/auth/signup.html",
@@ -24,6 +45,3 @@ angular.module('app', [
     });
 
 });
-
-
-
