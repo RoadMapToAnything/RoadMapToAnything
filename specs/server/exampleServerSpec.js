d476e1d53e3b3c933cc45a71cca0ace54a336394
delventhalz <delventhalz@gmail.com>
2016-01-21 14:55:39 -0600

    (test) Small update to testing config and example tests.

--- specs/server/exampleServerSpec.js
+++ specs/server/exampleServerSpec.js
@@ -3,6 +3,11 @@ var should = require('chai').should();
 
 describe('Testing the server', function() {
 
+  it('is expected to not have a window or a document', function() {
+    expect(typeof window).to.equal('undefined');
+    expect(typeof document).to.equal('undefined');
+  });
+
   it('should have a function called require', function() {
     require.should.exist;
     require.should.be.a('function');
