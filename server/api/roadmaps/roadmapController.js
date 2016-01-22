0fc29e6c59190461e2732c308cb410feec173bcf
Brendan Grady <cyhtan@gmail.com>
2016-01-22 14:12:59 -0500

    Add POST /api/roadmaps route handler

--- server/api/roadmaps/roadmapController.js
+++ server/api/roadmaps/roadmapController.js
@@ -3,7 +3,14 @@ var Roadmap = require('./roadmapModel.js');
 module.exports = {
 
   createRoadmap : function (req, res, next) {
-    
+    var newRoadmap = req.body;
+    Roadmap(newRoadmap).save()
+      .then(function(dbResults){
+        res.status(201).json(dbResults);
+      })
+      .catch(function(err){
+        next(err);
+      });
   },
   getRoadmaps : function (req, res, next) {
     
