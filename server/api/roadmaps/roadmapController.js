70c4057455a3df46ad1236b8ce89f1ae33345b43
Brendan Grady <cyhtan@gmail.com>
2016-01-22 03:08:49 -0500

    Add roadmap model and controller

---
+++ server/api/roadmaps/roadmapController.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+
+var RoadmapSchema = new mongoose.Schema({
+    title      : { type: String,   required: true },
+    description: { type: String,   required: true },
+    author     : { type: ObjectId, required: true, ref: 'User' },
+    nodes      : [ { type: ObjectId, ref: 'Node'} ],
+    created    : { type: Date, default: Date.now}
+});
+
+module.exports = mongoose.model('Roadmap', RoadmapSchema);
+
+// mongoose.Schema.ObjectId
