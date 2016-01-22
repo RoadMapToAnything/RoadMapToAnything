d432623eef4fd8d5c42b183127fefe0177bdb0f7
delventhalz <delventhalz@gmail.com>
2016-01-21 19:23:38 -0600

    (feat) Build framework for first sort query tests.

---
+++ specs/server/querySpec.js
@@ -0,0 +1,12 @@
+var expect = require('chai').expect;
+var url = 'http://localhost:3000/api/';
+
+describe('Query String Handler', function() {
+
+  describe('Sort parameter', function() {
+
+    it('should sort users by ')
+
+  });
+
+});
