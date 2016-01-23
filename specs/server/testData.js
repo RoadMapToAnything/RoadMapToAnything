22332bf56cd36d11620dcf227de5ce89fe99d3a8
delventhalz <delventhalz@gmail.com>
2016-01-23 08:54:28 -0600

    (refactor) Change test data somehow.

---
+++ specs/server/testData.js
@@ -0,0 +1,147 @@
+var User = require('../../server/api/users/userModel.js'),
+    Roadmap = require('../../server/api/roadmaps/roadmapModel.js'),
+    Node = require('../../server/api/nodes/nodeModel.js');
+
+var users = [
+  {
+    username: 'Bob',
+    password: 'c',
+    roadmaps: []
+  },
+
+  {
+    username: 'Susan', 
+    password: 'a',
+    roadmaps: []
+  },
+
+  {
+    username: 'Alejandro', 
+    password: 'b',
+    roadmaps: []
+  }
+];
+
+var maps = [
+  {
+    title      : 'Understanding Bowie',
+    description: 'This roadmap will help you learn about David Bowie',
+    nodes      : []
+  },
+
+  {
+    title      : 'Learning JavaScript',
+    description: '134rn 2 b 4n 1337 h4xx0rz',
+    nodes      : []
+  },
+
+  {
+    title      : 'Straight Outta Knowing Nothing About Straight Outta Compton',
+    description: 'Your hip hop history. Learn it.',
+    nodes      : []
+  }
+];
+
+var nodes = [
+  {
+    title        : 'Watch Life On Mars?',
+    description  : 'Watch this classic. Absorb the Bowie.',
+    resourceType : 'Music Video',
+    resourceURL  : 'https://www.youtube.com/watch?v=v--IqqusnNQ',
+    parentRoadmap: []
+  },
+
+  {
+    title        : 'Take Codecademy\'s Intro Course',
+    description  : 'OMG 1f u c4nt h4x th15 w4t h0p d0 u h4v???',
+    resourceType : 'Online Course',
+    resourceURL  : 'https://www.codecademy.com/learn/javascript',
+    parentRoadmap: []
+  },
+
+  {
+    title        : 'Watch Straight Outta Compton',
+    description  : 'It\'s in the name. Watch it.',
+    resourceType : 'Music Video',
+    resourceURL  : 'https://www.youtube.com/watch?v=TMZi25Pq3T8',
+    parentRoadmap: []
+  }
+];
+
+
+module.exports.seedUsers = function(next) {
+
+  var addUser = function(i) {
+    if (i >= users.length) {
+      if (next) next();
+      return;
+    }
+
+    User(users[i]).save()
+      .then(function (user) {
+        if (user) users[i] = user;
+        addUser(i + 1);
+      });
+  };
+
+  addUser(0);
+};
+
+
+module.exports.clearUsers = function(next) {
+
+  var clearUser = function(i) {
+    if (i >= maps.length) {
+      if (next) next();
+      return;
+    }
+
+    User.findOne(users[i])
+      .then(function (user) {
+        if (user) user.remove();
+        clearUser(i + 1);
+      });
+  };
+
+  clearUser(0);
+};
+
+
+module.exports.seedData = function(next) {
+
+  var addRoadmap = function(i) {
+    if (i >= maps.length) {
+      if (next) next();
+      return;
+    }
+
+    maps[i].author = users[i]._id;
+
+    Roadmap(maps[i]).save()
+      .then(function (map) {
+        if (map) maps[i] = map;
+        addRoadmap(i + 1);
+      });
+  };
+
+  module.exports.seedUsers(addRoadmap.bind(null, 0));
+};
+
+
+module.exports.clearData = function(next) {
+
+  var clearRoadmap = function(i) {
+    if (i >= maps.length) {
+      if (next) next();
+      return;
+    }
+
+    Roadmap.findOne(maps[i])
+      .then(function (map) {
+        if (map) map.remove();
+        clearRoadmap(i + 1);
+      });
+  };
+
+  module.exports.clearUsers(clearRoadmap.bind(null, 0));
+};
