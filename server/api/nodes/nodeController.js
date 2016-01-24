e8df1392bc4a37e3114fb4b13ced0ee258cb6c8e
Brendan Grady <cyhtan@gmail.com>
2016-01-23 19:47:46 -0500

    Add route handler for POST /nodes and /roadmaps/:roadmapID/nodes

--- server/api/nodes/nodeController.js
+++ server/api/nodes/nodeController.js
@@ -1,13 +1,30 @@
-var Node = require('./nodeModel.js');
+var Node        = require('./nodeModel.js'),
+    handleError = require('../../util.js').handleError,
+    handleQuery = require('../queryHandler.js');
 
 module.exports = {
+
   createNode : function (req, res, next) {
+    var newNode = req.body;
+    // Support /nodes and /roadmaps/roadmapID/nodes
+    newNode.parentRoadmap = newNode.parentRoadmap || req.params.roadmapID;
+    Node(newNode).save()
+      .then(function(dbResults){
+        res.status(201).json(dbResults);
+      })
+      .catch(handleError(next));
+  },
+
+  getNodes : function (req, res, next) {
 
   },
+
   updateNode : function (req, res, next) {
 
   },
+
   deleteNode : function (req, res, next) {
 
   }
+
 };
