ee34a773fa3aaa7baf892c9c55565e032b4de1ed
GMeyr <gabrielmeyr@gmail.com>
2016-01-23 21:12:02 -0500

    refactor helper functions for modularity

--- client/app/progress/dashboard.js
+++ client/app/progress/dashboard.js
@@ -1,33 +1,60 @@
-var request = require('request');
-
 angular.module('app.dash', [])
 
-.controller('DashboardController', function($scope){
-  $scope.userName = "not_set";
-  $scope.followedMaps = "unknown";
+.controller('DashboardController', ['$scope','$http', function($scope,$http){
+  $scope.username = "";
 
-  $scope.getFollowedMaps = function(){
-    console.log('calling getFollowedMaps');
-    if( process.env.NODE_ENV === 'development'){
-      request('localhost:3000/#/api/users/' + $scope.userName ), function (err, res, body){
-        if(err) {
-          console.log("error", err);
-        } else if ( res.statusCode == 200 ){
-          console.log("getFollowedMaps fail");
-        } else {
-          $scope.followedMapsResponseBody = body.roadmaps;
-        }
-      }
-    } else {
-      request('http://roadmaptoanything.herokuapp.com/#/api/users/' + $scope.userName ), function (err, res, body){
-        if(err) {
-          console.log("error", err);
-        } else if ( res.statusCode == 200 ){
-          console.log("getFollowedMaps fail");
-        } else {
-          $scope.followedMapsResponseBody = body.roadmaps;
-        }
-      };
-    }
+  $scope.followedMapsResponseBody = {};
+
+  $scope.myMapsResponseBody = {};
+
+  $scope.dummyFollowedMapsResponseBody = {
+    username: 'taylor',
+    followedMaps: [{
+
+    },{
+
+    },{
+
+    }]
   };
-});
+
+  $scope.dummyMyMapsResponseBody = {};
+
+  //getDashboardData();
+
+
+  // helper functions
+
+  function getDashboardData (){
+    angular.element(document).ready( function(){
+        getMyMaps();
+        getFollowedMaps();
+      });
+  }
+
+
+  function getMyMaps (){
+      console.log('calling getMyMaps');
+
+  //    $http.get('http://roadmaptoanything.herokuapp.com/#/api/users/' + $scope.userName )
+        $http.get('localhost:3000/#/api/users/' + $scope.userName )
+          .then(function(response){
+            $scope.myMapsResponseBody = response.body.roadmaps;
+            }, function(err){
+              console.log("error with MyMaps request", err);
+            });
+    }
+
+  function getFollowedMaps (){
+      console.log('calling getFollowedMaps');
+  //    $http.get('http://roadmaptoanything.herokuapp.com/#/api/users/' + $scope.userName )
+        $http.get('localhost:3000/#/api/users/' + $scope.userName )
+          .then(function(response){
+            $scope.followedMapsResponseBody = response.body.roadmaps;
+            }, function(err){
+              console.log('error with followedMaps request', err);
+            });
+    }
+}]);
+
+
