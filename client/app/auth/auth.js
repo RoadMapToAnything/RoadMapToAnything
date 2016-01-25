a20b54240431dc6643a335298acb2cd789f5b55b
GMeyr <gabrielmeyr@gmail.com>
2016-01-25 15:52:49 -0500

    add and style signin form

--- client/app/auth/auth.js
+++ client/app/auth/auth.js
@@ -1,8 +1,22 @@
 angular.module('app.auth', [])
 
-.controller('AuthController', function($scope){
+.controller('AuthController', ['$scope', '$http', function($scope, $http){
   $scope.test = function(){
     console.log('Auth controller is working');
   };
   $scope.test();
-});
+
+  $scope.attemptLogin = function () {
+    $http.get('/api/login?username=' + $scope.attemptedUsername + '&password=' + $scope.attemptedPassword )
+    .then(
+      //success callback
+      function(){
+        console.log('sent login credentials');
+      },
+      //error callback
+      function(err){
+        console.log('error sending credentials', err);
+      }
+      );
+  };
+}]);
--- client/app/auth/authController.js
+++
@@ -1,8 +0,0 @@
-angular.module('app.auth', [])
-
-.controller('AuthController', function($scope){
-  $scope.test = function(){
-    console.log('Main controller is working');
-  }
-  $scope.test();
-});
