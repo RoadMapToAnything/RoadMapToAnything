8b009943e9c2cd9951ff57e3b0d2ac7fa5923c3c
GMeyr <gabrielmeyr@gmail.com>
2016-01-25 19:25:40 -0500

    add signin tests

---
+++ specs/client/signinSpec.js
@@ -0,0 +1,47 @@
+describe('AuthController', function () {
+  var $scope, $rootScope, $location, createController, $httpBackend;
+
+  beforeEach(module('app'));
+  beforeEach(inject(function($injector) {
+
+    // mock out our dependencies
+    $rootScope = $injector.get('$rootScope');
+    $httpBackend = $injector.get('$httpBackend');
+    $location = $injector.get('$location');
+
+    $scope = $rootScope.$new();
+
+    var $controller = $injector.get('$controller');
+
+    createController = function () {
+      return $controller('AuthController', {
+        $scope: $scope
+      });
+    };
+
+    createController();
+  }));
+
+  xit('should have an attemptLogin function', function () {
+    expect($scope.attemptLogin).to.be.a('function');
+  });
+
+
+  xit('should get a 401 when credentials bad', function () {
+    $scope.attemptedUsername = 'noone-should-ever-pick-this-name';
+    $scope.attemptedLogin = 'or-this-password!#$';
+    return $scope.attemptLogin()
+      .then(function(response){
+        expect(response.status).to.be.equal.to(401);
+      });
+  });
+
+  xit('should get a 200 when credentials good', function () {
+    $scope.attemptedUsername = 'a';
+    $scope.attemptedLogin = 'a';
+    return $scope.attemptLogin()
+      .then(function(response){
+        expect(response.status).to.be.equal.to(200);
+      });
+  });
+});
