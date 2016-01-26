0bb07c2934270f8bae1cff282b61995b7dc2305a
GMeyr <gabrielmeyr@gmail.com>
2016-01-25 20:26:51 -0500

    build signup html page

--- client/app/auth/auth.js
+++ client/app/auth/auth.js
@@ -1,8 +1,9 @@
 angular.module('app.auth', [])
 
 .controller('AuthController', ['$scope', '$http', function($scope, $http){
-<<<<<<< HEAD
 
+  $scope.attemptedFirstName = "not yet set";
+  $scope.attemptedLastName = "not yet set";
   $scope.attemptedUsername = "not yet set";
   $scope.attemptedPassword = "not yet set";
 
@@ -31,24 +32,31 @@ angular.module('app.auth', [])
       }
       );
   };
-=======
-  $scope.test = function(){
-    console.log('Auth controller is working');
-  };
-  $scope.test();
 
-  $scope.attemptLogin = function () {
-    $http.get('/api/login?username=' + $scope.attemptedUsername + '&password=' + $scope.attemptedPassword )
+  $scope.attemptSignup = function () {
+    $scope.showUnauthMsg = false;
+    $http.post('/api/signup', {
+      firstName: $scope.attemptedFirstName,
+      lastName: $scope.attemptedLastName,
+      username: $scope.attemptedUsername,
+      password: $scope.attemptedPassword
+    })
     .then(
       //success callback
-      function(){
+      function(res){
         console.log('sent login credentials');
+        console.log('res.data', res.data);
+        res.redirect('/#/dashboard?username=' + res.body.username);
       },
       //error callback
-      function(err){
-        console.log('error sending credentials', err);
+      function(res){
+        console.log('res:', res);
+        if(res.status === 401){
+          console.log('unauthorized credentials');
+          $scope.showUnauthMsg = true;
+        }
       }
       );
   };
->>>>>>> add and style signin form
+  
 }]);
