31fa41a8bbae364c991b19e0b39dfdb1ab6fb609
GMeyr <gabrielmeyr@gmail.com>
2016-01-26 22:19:40 -0500

    rebase auth conflict

--- client/app/auth/auth.js
+++ client/app/auth/auth.js
@@ -56,5 +56,28 @@ angular.module('app.auth', [])
       }
     );
   };
+<<<<<<< HEAD
   
+=======
+=======
+  $scope.test = function(){
+    console.log('Auth controller is working');
+  };
+  $scope.test();
+
+  $scope.attemptLogin = function () {
+    $http.get('/api/login?username=' + $scope.attemptedUsername + '&password=' + $scope.attemptedPassword )
+    .then(
+      //success callback
+      function(){
+        console.log('sent login credentials');
+      },
+      //error callback
+      function(err){
+        console.log('error sending credentials', err);
+      }
+      );
+  };
+>>>>>>> add and style signin form
+>>>>>>> add and style signin form
 }]);
