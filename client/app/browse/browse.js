c1b60f400b0a202e6e9a0547068f2d5471cdafae
GMeyr <gabrielmeyr@gmail.com>
2016-01-27 14:45:28 -0500

    hook up to api for map data results

--- client/app/browse/browse.js
+++ client/app/browse/browse.js
@@ -1,6 +1,34 @@
 angular.module('app.browse', [])
 
 .controller('BrowseController', ['$scope', '$http','$state', function($scope, $http, $state){
+  $scope.mapData = [];
+  
+  // this fn should be refactored to a factory -- a variattion is used by dashboard
+  $scope.addTotalNodesOfMaps = function (arr){
+    arr.forEach(function(map){
+      map.totalNodes = map.nodes.length;
+    });
+  };
 
+  $scope.getMapData = function () {
+    $http({
+      method: 'GET',
+      url:'/api/roadmaps?sort=-created'
+    })
+      .then(
+        //success callback
+        function(response){
+          console.log('followed response.data', response.data);
+          $scope.mapData = response.data.data || [];
+          $scope.addTotalNodesOfMaps($scope.mapData);
+        },
+        //failure callback
+        function(err){
+          console.log('error with getMapData request', err);
+        }
+      );
+    };
+
+  $scope.getMapData();
 
 }]);
