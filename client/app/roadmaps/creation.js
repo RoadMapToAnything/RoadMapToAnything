89804cd77af821e13c7cdf902c1ddfb148bac675
delventhalz <delventhalz@gmail.com>
2016-01-28 15:08:34 -0600

    (cleanup) Clean up creation.js to remove debugging tools.

--- client/app/roadmaps/creation.js
+++ client/app/roadmaps/creation.js
@@ -1,17 +1,13 @@
 angular.module('app.creation', [])
 
-.controller('CreationController', function($scope,$http){
-  localStorage.removeItem('user.currentRoadMap');
+.controller('CreationController', function($scope, $http){
 
-  // This is for the roadMap Creation Form
-  // $scope.nodeBuilder = false;
-  // $scope.checkTruth = function(){
-  //   return $scope.nodeBuilder;
-  // };
+  // As user will be building a brand new roadmap, the currently  
+  // active one is removed from local storage.
+  localStorage.removeItem('user.currentRoadMap');
 
   var buildRoadmap = function() {
     author = localStorage.getItem('user.username') || 'bowieloverx950';
-    console.log('BUILD ROADMAP W/ AUTHOR', author);
 
     return {
       title: $scope.roadmapTitle,
@@ -21,24 +17,22 @@ angular.module('app.creation', [])
   };
 
   var postRoadmap = function(roadmap) {
-    console.log('POSTING ROADMAP', roadmap);
 
    return $http({
       method: 'POST',
       url: '/api/roadmaps',
       data: roadmap
     }).then(function (res) {
-      console.log('Roadmap created:', res.data.data);
       localStorage.setItem('user.currentRoadMap', res.data.data._id);
-      // $scope.nodeBuilder = true;
+      console.log('Roadmap created:', res.data.data);
     }, function(err){
       if (err) return err;
     });
+
   };
 
   var buildNode = function() {
     var parent = localStorage.getItem('user.currentRoadMap');
-    console.log('BUILD NODE W/ PARENT', parent);
 
     return {
       title: $scope.nodeTitle,
@@ -51,7 +45,7 @@ angular.module('app.creation', [])
   };
 
   var postNode = function(node) {
-    console.log('POSTING NODE', node);
+
     return $http({
       method: 'POST',
       url: '/api/nodes',
@@ -60,6 +54,7 @@ angular.module('app.creation', [])
     .then(function (res) {
       console.log('Node created:', res.data.data);
     });
+
   };
 
   $scope.submitAndRefresh = function() {
