d4dc39c267ad13a92d38b4c601c153bda54c68d5
delventhalz <delventhalz@gmail.com>
2016-01-28 21:07:01 -0600

    (fix) Fix create roadmap page redirecting before new node was set in local storage.

--- client/app/roadmaps/creation.js
+++ client/app/roadmaps/creation.js
@@ -1,6 +1,6 @@
 angular.module('app.creation', [])
 
-.controller('CreationController', function($scope, $http){
+.controller('CreationController', function($scope, $http, $state){
 
   // As user will be building a brand new roadmap, the currently  
   // active one is removed from local storage.
@@ -60,8 +60,18 @@ angular.module('app.creation', [])
   };
 
   $scope.submitAndRefresh = function() {
-    Materialize.updateTextFields();
-    $scope.submitNode();
+    $scope.submitNode()
+    .then(function() {
+      Materialize.updateTextFields();
+    })
+    
+  };
+
+  $scope.submitAndExit = function() {
+    $scope.submitNode()
+    .then(function() {
+      $state.go('roadmapTemplate');
+    });
     
   };
 
@@ -71,11 +81,11 @@ angular.module('app.creation', [])
       postRoadmap(buildRoadmap())
       .then(function (err) {
         if (err) return console.log(err);
-        postNode(buildNode());
+        return postNode(buildNode());
       });
 
     } else {
-      postNode(buildNode());
+      return postNode(buildNode());
     }
   };
 
