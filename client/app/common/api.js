b2713037b89f8143abbc79ce54b3efcf63441f16
delventhalz <delventhalz@gmail.com>
2016-01-29 16:30:20 -0600

    (feat) Add an variety of user methods to API factory

--- client/app/common/api.js
+++ client/app/common/api.js
@@ -7,7 +7,77 @@ angular.module('app.common', [])
     var user = localStorage.getItem('user.username');
     var token = localStorage.getItem('user.authToken');
     return btoa(user + ':' + token);
-  }
+  };
+
+  var standardResponse = function(res) {
+    var nameKeys = ['title', 'username'];
+    var name;
+
+    // Grab the name of the item affected from response
+    nameKeys.forEach(function (key) {
+      if (res.data.data[key]) name = res.data.data[key];
+    });
+
+    console.log(res.status, '-', 
+      res.config.method, 'successful for', 
+      name, ':', res.data.data);
+
+    return res.data.data;
+  };
+
+  var standardError = function(err) {
+    console.log(err);
+  };
+
+
+  /* * * * * * * * * * * * * * * * * * * * * 
+   *                 USERS                 *
+   * * * * * * * * * * * * * * * * * * * * */
+
+  api.getUsers = function() {
+
+    return $http.get('/api/users')
+    .then(standardResponse)
+    .catch(standardError);
+  };
+
+  api.getUserByUsername = function(username) {
+
+    return $http.get('/api/users/' + username)
+    .then(standardResponse)
+    .catch(standardError);
+  };
+
+  api.getUser = api.getUserByUsername;
+
+  api.updateUser = function(user) {
+
+    return $http({
+      method: 'PUT',
+      url: '/api/users/' + user.username,
+      data: user,
+      headers: { Authorization: 'Basic ' + encodeAuthHeader() }
+    })
+    .then(standardResponse)
+    .catch(standardError);
+  };
+
+  api.deleteUser = function(username) {
+
+    return $http({
+      method: 'PUT',
+      url: '/api/users/' + user.username,
+      data: user,
+      headers: { Authorization: 'Basic ' + encodeAuthHeader() }
+    })
+    .then(standardResponse)
+    .catch(standardError);
+  };
+
+
+  /* * * * * * * * * * * * * * * * * * * * * 
+   *               ROADMAPS                *
+   * * * * * * * * * * * * * * * * * * * * */
 
   api.saveRoadmap = function(roadmap) {
 
@@ -16,14 +86,12 @@ angular.module('app.common', [])
       url: '/api/roadmaps',
       data: roadmap,
       headers: { Authorization: 'Basic ' + encodeAuthHeader() }
-    }).then(function (res) {
-      console.log('Roadmap saved:', res.data.data);
-      return res.data.data;
-    }).catch(function (err) {
-      console.log(err);
-    });
-
+    })
+    .then(standardResponse)
+    .catch(standardError);
   };
 
+
   return api;
+
 }]);
