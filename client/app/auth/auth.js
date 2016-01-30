a1deaa35b200bb390ebf97c1ab2df2b3f7975f34
delventhalz <delventhalz@gmail.com>
2016-01-29 20:41:08 -0600

    (refactor) Implement user factory in auth controller

--- client/app/auth/auth.js
+++ client/app/auth/auth.js
@@ -1,6 +1,6 @@
-angular.module('auth.ctrl', [])
+angular.module('auth.ctrl', ['services.user'])
 
-.controller('AuthController', ['$scope', '$http','$state', function($scope, $http, $state){
+.controller('AuthController', ['$scope','$state', 'User' function($scope, $state, User){
 
   $scope.attemptedFirstName = '';
   $scope.attemptedLastName = '';
@@ -11,50 +11,35 @@ angular.module('auth.ctrl', [])
 
   $scope.attemptLogin = function () {
     $scope.showUnauthMsg = false;
-    $http({
-      url: '/api/login?username=' + $scope.attemptedUsername + '&password=' + $scope.attemptedPassword,
-      method: 'GET'
+
+    User.login($scope.attemptedUsername, $scope.attemptedPassword)
+    .then(function (data) {
+      $state.go('dashboard');
     })
-    .then(
-      //success callback
-      function(res){
-        console.log('Logged in with response data:', res.data);
-        localStorage.setItem('user.username', $scope.attemptedUsername);
-        localStorage.setItem('user.authToken', res.data.data.authToken);
-        $state.go('dashboard');
-      },
-      //error callback
-      function(res){
-        console.log('res:', res);
-        if(res.status === 401){
-          console.log('Login failed: unauthorized credentials.');
-          $scope.showUnauthMsg = true;
-        }
+    .catch(function (err) {
+      if (err.status === 401) {
+        console.log('(401) Login failed: bad credentials.');
+        $scope.showUnauthMsg = true;
       }
-    );
+    });
   };
 
   $scope.attemptSignup = function () {
     $scope.showUnauthMsg = false;
-    $http.post('/api/signup', {
+
+    User.signup({
       firstName: $scope.attemptedFirstName,
       lastName: $scope.attemptedLastName,
       username: $scope.attemptedUsername,
       password: $scope.attemptedPassword
     })
-    .then(
-      //success callback
-      function(res){
-        console.log($scope.attemptedUsername, 'successfully signed up.');
-        localStorage.setItem('user.username', $scope.attemptedUsername);
-        $state.go('dashboard');
-      },
-      //error callback
-      function(err){
-        console.log('Login failed:', err);
-        $scope.showUnauthMsg = true;
-      }
-    );
+    .then(function (res) {
+      $state.go('dashboard');
+    })
+    .catch(function (err) {
+      console.log('Login failed:', err);
+      $scope.showUnauthMsg = true;
+    });
   };
 
 }]);
