7f7b67cad6fa04fe8adffd8f15de30bb88b851c2
GMeyr <gabrielmeyr@gmail.com>
2016-01-30 18:45:25 -0500

    (refactor) creating maps/nodes now doesn't use localStorage for mapID

--- client/app/roadmaps/creation.js
+++ client/app/roadmaps/creation.js
@@ -2,22 +2,17 @@ angular.module('creation.ctrl', ['services.server'])
 
 .controller('CreationController', ['$scope', '$state', 'Server', function($scope, $state, Server){
 
-  // As user will be building a brand new roadmap, so the   
-  // currently active one must be  removed from local storage
-  localStorage.removeItem('roadmap.id');
-
   var createRoadmap = function() {
     return Server.createRoadmap({
       title: $scope.roadmapTitle,
       description: $scope.roadmapDescription
     })
     .then(function (roadmap) {
-      localStorage.setItem('roadmap.id', roadmap._id);
+      $scope.roadmapID = roadmap._id;
     });
   };
 
-  var createNode = function() {
-    var parent = localStorage.getItem('roadmap.id');
+  var createNode = function() {;
 
     return Server.createNode({
       title: $scope.nodeTitle,
@@ -25,13 +20,13 @@ angular.module('creation.ctrl', ['services.server'])
       resourceType: $scope.nodeType,
       resourceURL: $scope.nodeUrl,
       imageUrl: $scope.nodeImageUrl,
-      parentRoadmap: parent
+      parentRoadmap: $scope.roadmapID
     });
   };
 
   // Creates the roadmap before the node if necessary
   var checkThenCreate = function() {
-    if (!localStorage.getItem('roadmap.id')) {
+    if (!$scope.roadmapID) {
       return createRoadmap().then(function() {
         return createNode();
       });
@@ -49,7 +44,7 @@ angular.module('creation.ctrl', ['services.server'])
 
   $scope.submitAndExit = function() {
     checkThenCreate().then(function() {
-      $state.go('roadmapTemplate');
+      $state.go('home.roadmapTemplate', { 'roadmapID': $scope.roadmapID });
     });
   };
 
