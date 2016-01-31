7138dcabb1561266266922be6b6f67010e949377
delventhalz <delventhalz@gmail.com>
2016-01-31 10:32:07 -0600

    (test) Finish users spec refactor

--- specs/server/usersSpec.js
+++ specs/server/usersSpec.js
@@ -153,79 +153,80 @@ describe('The users API', function() {
 
     before(function(done) {
       request(server.app)
-      .get(route)
+      .get(route + '/' + name)
       .expect('Content-Type', /json/)
       .expect(200)
       .end(function (err, res) {
         user = res.body.data;
         done();
-
       });
+    });
+
+    it('Should retrieve a specifc user', function() {
+      expect(user).to.have.property('username', name);
+      expect(user).to.have.property('firstName', testUser.firstName);
+      expect(user).to.have.property('lastName', testUser.lastName);
+    });
+
+    it('Should have a populated authored roadmaps array', function() {
+      var maps = user.authoredRoadmaps;
+
+      expect(maps).to.be.an('array');
+      expect(maps).to.not.be.empty;
+      expect(maps[0]).to.have.property('title', testMap.title);
+    });
+
+    it('Should populate its authored roadmaps', function() {
+      var nodes = user.authoredRoadmaps[0].nodes;
+
+      expect(nodes).to.be.an('array');
+      expect(nodes).to.not.be.empty;
+      expect(nodes[0]).to.have.property('title', testNode.title);
     });
 
   });
 
-  describe('Fetching Users', function() {
-    var username = testUser.username;
+  describe('Updating a User', function() {
+    var name = testUser.username;
+    var timestamp;
+
+    before(function(done) {
+      User.findOne({username: name})
+      .then(function (user) {
+        timestamp = user.updated;
+        done();
+      });
+    });
 
     after(function(done) {
-      User.findOneAndUpdate({username: username}, {firstName: testUser.firstName})
+      User.findOneAndUpdate({username: name}, {firstName: testUser.firstName})
       .then(function() {
         done();
       });
     });
 
-    it('should retrieve a specific user with name properties, timestamps, and populated authoredRoadmaps', function (done) {
-
+    it('Should update a user, updating the timestamp', function(done) {
       request(server.app)
-        .get(route + '/' + username)
-        .expect('Content-Type', /json/)
-        .expect(200)
-        .end(function (err, res) {
+      .put(route + '/' + name)
+      .send({firstName: 'Robert'})
+      .expect('Content-Type', /json/)
+      .expect(201)
+      .end(function (err, res) {
 
-          expect(res.body.data).to.have.property('username', username);
-          expect(res.body.data).to.have.property('firstName', testUser.firstName);
-          expect(res.body.data).to.have.property('lastName', testUser.lastName);
-          expect(res.body.data).to.have.property('created');
-          expect(res.body.data).to.have.property('updated');
-          expect(res.body.data).to.have.property('authoredRoadmaps');
-          expect(res.body.data.authoredRoadmaps).to.be.an('array');
-          expect(res.body.data.authoredRoadmaps).to.not.be.empty;
-          expect(res.body.data.authoredRoadmaps[0]).to.have.property('title');
+        expect(res.body.data).to.have.property('username', name);
 
+        User.findOne({username: name})
+        .then(function (user) {
+
+          expect(user).to.have.property('firstName', 'Robert');
+          expect(user.created).to.not.equal(user.updated);
+
+          // Timestamps must be wrapped in order to ensure a consistent format.
+          expect( new Date(user.updated).getTime() ).to.not.equal( new Date(timestamp).getTime() );
           done();
         });
 
-    });
-
-
-    it('should update a user with new first name and timestamps', function (done) {
-      User.findOne({username: username})
-        .then(function (user) {
-          var preUpdateStamp = user.updated;
-
-          request(server.app)
-            .put(route + '/' + username)
-            .send({firstName: 'Robert'})
-            .expect('Content-Type', /json/)
-            .expect(201)
-            .end(function (err, res) {
-
-              expect(res.body.data).to.have.property('username', username);
-              User.findOne({username: username})
-                .then(function (user) {
-
-                  expect(user).to.have.property('firstName', 'Robert');
-                  expect(user.created).to.not.equal(user.updated);
-
-                  // Timestamps must be wrapped in order to ensure a consistent format.
-                  expect( new Date(user.updated).getTime() ).to.not.equal( new Date(preUpdateStamp).getTime() );
-                  done();
-                });
-
-            });
-
-        });
+      });
 
     });
 
