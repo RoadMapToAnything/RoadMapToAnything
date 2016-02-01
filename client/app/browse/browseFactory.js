9a0ded7668df802bd07ba76788537bfc121a44d1
delventhalz <delventhalz@gmail.com>
2016-02-01 13:32:29 -0600

    (refactor) Add a few semi-colons

--- client/app/browse/browseFactory.js
+++ client/app/browse/browseFactory.js
@@ -17,25 +17,25 @@ angular.module('browse.factory', ['services.server'])
       console.log(data);
       cb(data);
     });
-  }
+  };
 
   var addMapToEmbarked = function(mapID) {
     var username = localStorage.getItem('user.username');
     var user = {'user.username': username, 'inProgress.roadmaps': mapID }
     Server.updateUser(user);
-  }
+  };
 
   var gotToMap = function(mapID){
     localStorage.setItem('roadmap.id', mapID);
     $state.go('home.roadmapTemplate');
-  }
+  };
 
   return {
     addTotalNodesOfMaps: addTotalNodesOfMaps,
     getMapData: getMapData,
     addMapToEmbarked: addMapToEmbarked,
     gotToMap: gotToMap
-  }
+  };
   
 
 }])
