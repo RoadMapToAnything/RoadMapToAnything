a06322f8dffc0c773b7b888b8d9175812e1df0ae
Irene K <linqwn@gmail.com>
2016-02-01 22:48:22 -0800

    (feat) Add http requests methods to roadmap factory

--- client/app/roadmaps/roadmapsFactory.js
+++ client/app/roadmaps/roadmapsFactory.js
@@ -2,7 +2,31 @@ angular.module('roadmaps.factory', [])
 
   .factory('RoadMapsFactory', function($http){
 
+    // Votes count to be displayed on roadmap page
+    var upvotes;
+    var downvotes;
+
+    var sendUpVote = function(roadmapId, username) {
+      // Send a data boolean in the body?
+      // Post request to database to update roadmap model to include username in upvotes array...
+       return $http.post('/api/roadmaps/' + id);
+       //.then?
+       //.catch?
+    }
+
+    var sendDownVote = function(roadmapId, username) {
+      // Send a data boolean in the body?
+      // Post request to database to update roadmap model to include username in downvotes array...
+       return $http.post('/api/roadmaps/' + id);
+       //.then?
+       //.catch?
+    }
+
+
     return {
-    
+      upvotes : upvotes,
+      downvotes : downvotes,
+      sendUpVote : sendUpVote,
+      sendDownVote : sendDownVote
     };
    });
