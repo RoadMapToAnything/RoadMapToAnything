7389b96aa19c47bf6e1e20e0bedfbb09672c1c56
GMeyr <gabrielmeyr@gmail.com>
2016-02-03 17:44:57 -0500

    (fix) Reset input values on login and signup attempt

--- client/app/auth/auth.js
+++ client/app/auth/auth.js
@@ -2,22 +2,39 @@ angular.module('auth.ctrl', ['services.user'])
 
 .controller('AuthController', ['$scope','$state', 'User', function($scope, $state, User){
 
-  $scope.attemptedFirstName = '';
-  $scope.attemptedLastName = '';
-  $scope.attemptedUsername = '';
-  $scope.attemptedPassword = '';
+  $scope.signin = true;
+  $scope.showSignup = function (){
+    $scope.signin = false;
+    $scope.resetInputs();
+  }
+  $scope.showSignin = function (){
+    $scope.signin = true;
+    $scope.resetInputs();
+  }
+  $scope.resetInputs = function() {
+    console.log("resetting inputs");
+    $scope.attemptedFirstName = '';
+    $scope.attemptedLastName = '';
+    $scope.attemptedUsername = '';
+    $scope.attemptedPassword = '';
+  }
+  $scope.resetInputs();
 
   $scope.showUnauthMsg = false;
 
   $scope.attemptLogin = function () {
     $scope.showUnauthMsg = false;
+    $scope.signin = true;
+
 
     User.login($scope.attemptedUsername, $scope.attemptedPassword)
     .then(function (data) {
+      $scope.resetInputs();
       $state.go('home.dashboard');
     })
     .catch(function (err) {
       if (err.status === 401) {
+        $scope.resetInputs();
         console.log('(401) Login failed: bad credentials.');
         $scope.showUnauthMsg = true;
       }
@@ -34,9 +51,11 @@ angular.module('auth.ctrl', ['services.user'])
       password: $scope.attemptedPassword
     })
     .then(function (res) {
+      $scope.resetInputs();
       $state.go('home.dashboard');
     })
     .catch(function (err) {
+      $scope.resetInputs();
       console.log('Login failed:', err);
       $scope.showUnauthMsg = true;
     });
