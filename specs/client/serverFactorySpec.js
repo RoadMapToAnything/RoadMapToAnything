4c0e50371132f518771b870c1acce69c6d127ab6
delventhalz <delventhalz@gmail.com>
2016-02-02 21:42:51 -0600

    (test) Finish tests for server factory

--- specs/client/serverFactorySpec.js
+++ specs/client/serverFactorySpec.js
@@ -2,7 +2,14 @@
 
 describe('Server Factory', function () {
   var $scope, $rootScope, $location, $window, $httpBackend, createController, Server;
+  var testId = '0000000010';
   var username = 'user';
+  var data = {
+      _id: testId,
+      username: username,
+      firstName: 'Gal',
+      authoredRoadmaps:[testId]
+    };
   var token = 'sjj232hwjhr3urw90rof';
 
   // using angular mocks, we can inject the injector
@@ -34,19 +41,12 @@ describe('Server Factory', function () {
    * * * * * * * * * * * * * * * * * * * * */
 
   describe('Users Methods', function() {
-    var testId = '0000000010';
-    var data = {
-      _id: testId,
-      username: username,
-      firstName: 'Gal',
-      authoredRoadmaps:[testId]
-    };
 
     it('Should have a getUsers method', function() {
       expect(Server.getUsers).to.be.a('function');
     });
 
-    it('Should send the proper GET request', function() {
+    it('Should send the right GET request', function() {
       var response;
       $httpBackend.expectGET('/api/users').respond({data: data});
       Server.getUsers().then(function (res) {
@@ -61,7 +61,7 @@ describe('Server Factory', function () {
       expect(Server.getUser).to.be.a('function');
     });
 
-    it('Should send a GET request for the proper user', function() {
+    it('Should send a GET request for the right user', function() {
       var response;
       $httpBackend.expectGET('/api/users/' + data.username).respond({data: data});
       Server.getUserByUsername(data.username).then(function (res) {
@@ -75,7 +75,7 @@ describe('Server Factory', function () {
       expect(Server.updateUser).to.be.a('function');
     });
 
-    it('Should send a PUT request for the proper user with the right data', function() {
+    it('Should send a PUT request for the right user with the right data', function() {
       var response;
       $httpBackend.expectPUT('/api/users/' + data.username, data).respond({data: data});
       Server.updateUser(data).then(function (res) {
@@ -90,7 +90,7 @@ describe('Server Factory', function () {
       expect(Server.deleteUser).to.be.a('function');
     });
 
-    it('Should send a DELETE request for the proper user', function() {
+    it('Should send a DELETE request for the right user', function() {
       var response;
       $httpBackend.expectDELETE('/api/users/' + data.username).respond({data: data});
       Server.deleteUserByUsername(data.username).then(function (res) {
@@ -99,13 +99,16 @@ describe('Server Factory', function () {
       $httpBackend.flush();
       expect(response).to.deep.equal(data);
     });
+  });
 
 
   /* * * * * * * * * * * * * * * * * * * * * 
    *           ROADMAPS METHODS            *
    * * * * * * * * * * * * * * * * * * * * */
 
-    it('Should have a getRoadmaps method', function() {
+  describe('Roadmaps Methods', function() {
+
+    it('Should have getRoadmaps methods', function() {
       expect(Server.getRoadmaps).to.be.a('function');
       expect(Server.getMaps).to.be.a('function');
     });
@@ -120,14 +123,14 @@ describe('Server Factory', function () {
       expect(response).to.deep.equal(data);
     });
 
-    it('Should have a getRoadmaps method', function() {
+    it('Should have get roadmap methods', function() {
       expect(Server.getRoadmapById).to.be.a('function');
       expect(Server.getMapById).to.be.a('function');
       expect(Server.getRoadmap).to.be.a('function');
       expect(Server.getMap).to.be.a('function');
     });
 
-    it('Should send a GET request for the proper roadmap', function() {
+    it('Should send a GET request for the right roadmap', function() {
       var response;
       $httpBackend.expectGET('/api/roadmaps/' + testId).respond({data: data});
       Server.getRoadmapById(testId).then(function (res) {
@@ -137,12 +140,27 @@ describe('Server Factory', function () {
       expect(response).to.deep.equal(data);
     });
 
-    it('Should have an updateRoadmap method', function() {
+    it('Should have create roadmap methods', function() {
+      expect(Server.createRoadmap).to.be.a('function');
+      expect(Server.createMap).to.be.a('function');
+    });
+
+    it('Should send a POST request with the right data', function() {
+      var response;
+      $httpBackend.expectPOST('/api/roadmaps', data).respond({data: data});
+      Server.createRoadmap(data).then(function (res) {
+        response = res;
+      });
+      $httpBackend.flush();
+      expect(response).to.deep.equal(data);
+    });
+
+    it('Should have a update roadmap methods', function() {
       expect(Server.updateRoadmap).to.be.a('function');
       expect(Server.updateMap).to.be.a('function');
     });
 
-    it('Should send a PUT request for the proper roadmap, with the right data', function() {
+    it('Should send a PUT request for the right roadmap, with the right data', function() {
       var response;
       $httpBackend.expectPUT('/api/roadmaps/' + data._id, data).respond({data: data});
       Server.updateRoadmap(data).then(function (res) {
@@ -152,17 +170,83 @@ describe('Server Factory', function () {
       expect(response).to.deep.equal(data);
     });
 
-    it('Should have delte roadmaps methods', function() {
+    it('Should have delete roadmap methods', function() {
       expect(Server.deleteRoadmapById).to.be.a('function');
       expect(Server.deleteMapById).to.be.a('function');
       expect(Server.deleteRoadmap).to.be.a('function');
       expect(Server.deleteMap).to.be.a('function');
     });
 
-    it('Should send a DELETE request for the proper roadmap', function() {
+    it('Should send a DELETE request for the right roadmap', function() {
       var response;
       $httpBackend.expectDELETE('/api/roadmaps/' + testId).respond({data: data});
       Server.deleteRoadmapById(testId).then(function (res) {
+        response = res;
+      });
+      $httpBackend.flush();
+      expect(response).to.deep.equal(data);
+    });
+  });
+
+
+  /* * * * * * * * * * * * * * * * * * * * * 
+   *             NODES METHODS             *
+   * * * * * * * * * * * * * * * * * * * * */
+
+  describe('Nodes Methods', function() {
+
+    it('Should have get node methods', function() {
+      expect(Server.getNodeById).to.be.a('function');
+      expect(Server.getNode).to.be.a('function');
+    });
+
+    it('Should send a GET request for the right node', function() {
+      var response;
+      $httpBackend.expectGET('/api/nodes/' + testId).respond({data: data});
+      Server.getNodeById(testId).then(function (res) {
+        response = res;
+      });
+      $httpBackend.flush();
+      expect(response).to.deep.equal(data);
+    });
+
+    it('Should have a create node method', function() {
+      expect(Server.createNode).to.be.a('function');
+    });
+
+    it('Should send a POST request with the right data', function() {
+      var response;
+      $httpBackend.expectPOST('/api/nodes', data).respond({data: data});
+      Server.createNode(data).then(function (res) {
+        response = res;
+      });
+      $httpBackend.flush();
+      expect(response).to.deep.equal(data);
+    });
+
+    it('Should have an update node method', function() {
+      expect(Server.updateNode).to.be.a('function');
+    });
+
+    it('Should send a PUT request for the right node with the right data', function() {
+      var response;
+      $httpBackend.expectPUT('/api/nodes/' + data._id, data).respond({data: data});
+      Server.updateNode(data).then(function (res) {
+        response = res;
+      });
+      $httpBackend.flush();
+      expect(response).to.deep.equal(data);
+    });
+
+    it('Should have delete node method', function() {
+      expect(Server.deleteNodeById).to.be.a('function');
+      expect(Server.deleteNode).to.be.a('function');
+    });
+
+    it('Should send a DELETE request for the right node', function() {
+      var response;
+      $httpBackend.expectDELETE('/api/nodes/' + testId).respond({data: data});
+      Server.deleteNodeById(testId).then(function (res) {
         response = res;
       });
       $httpBackend.flush();
