c2dadda21a93e0efb863a344052d247a95497744
delventhalz <delventhalz@gmail.com>
2016-02-06 13:37:54 -0600

    (feat) Implement scraper live test at /scraper

--- client/app/scraper/scraper.js
+++ client/app/scraper/scraper.js
@@ -2,12 +2,22 @@ angular.module('scraper.ctrl', ['services.server'])
 
 
 .controller('ScraperController', [ '$scope', 'Server', function($scope, Server){
+  $scope.urlToScrape = '';
+  $scope.displayUrl = '';
+  $scope.scrape = {};
 
-  $('#scrape-results').append('Hello World!');
 
-  Server.scrape('http://www.dcbeekeepers.org/')
-  .then(function (data) {
-    console.log('TEST', data);
-  });
+  $scope.checkAndSubmit = function() {
+    var url = $scope.urlToScrape;
+    if (url.length < 4) return;
+    if (url.substring(0, 4) !== 'http') url = 'http://' + url;
+
+    $scope.displayUrl = url;
+
+    Server.scrape(url)
+    .then(function (data) {
+      $scope.scrape = data;
+    });
+  };
 
 }]);
