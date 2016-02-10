15ea287fd8fa92d55e7a57955c6cedaa76407b10
delventhalz <delventhalz@gmail.com>
2016-02-10 14:27:19 -0600

    (feat) Add upvoting and downvoting to browse view

--- client/app/browse/browseFactory.js
+++ client/app/browse/browseFactory.js
@@ -4,31 +4,22 @@ angular.module('browse.factory', ['services.server'])
   var mapData =[];
   var showSigninMsg = false;
 
-  var addTotalNodesOfMaps = function (arr){
-    arr.forEach(function(map){
-      map.totalNodes = map.nodes.length;
-    });
-  };
-
   var getMapData = function(cb){
     var mapData = Server.getRoadmaps({sort: '-bestRating'})
     .then(function(data){
-      addTotalNodesOfMaps(data);
-      console.log(data);
       cb(data);
     });
   };
 
   var orderMaps = function(mapData, criteria){
     if (!criteria){
-      criteria = 'created'
+      criteria = 'created';
     }
-    return mapData[criteria]
+    return mapData[criteria];
     
   }
 
   return {
-    addTotalNodesOfMaps: addTotalNodesOfMaps,
     getMapData: getMapData,
     orderMaps: orderMaps
   };
