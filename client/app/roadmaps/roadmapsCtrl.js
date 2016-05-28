f0ffd5e71c48efdf5add90c80123f80e3da73195
delventhalz <delventhalz@gmail.com>
2016-05-28 13:16:15 -0500

    Add vertical rule, horizontal fab, and image card

--- client/app/roadmaps/roadmapsCtrl.js
+++ client/app/roadmaps/roadmapsCtrl.js
@@ -3,5 +3,14 @@ angular.module('roadmaps.ctrl', ['roadmaps.factory', 'services.server', 'service
 .controller('RoadmapsController', [ '$scope', '$stateParams', 'RoadmapsFactory', 'Server', 'User', '$timeout', '$state', function($scope, $stateParams, RoadmapsFactory, Server, User, $timeout, $state){  
   angular.extend($scope, RoadmapsFactory);
 
+  // Fetch the roadmap from the server and attach it to $scope
+  Server.getMap($stateParams.roadmapID)
+  .then(function(map) {
+    $scope.map = map;
+  });
+
+  $scope.selectNode = function(node) {
+    $scope.selected = node;
+  };
 
 }]);
--- client/app/roadmaps/roadmapsFactory.js
+++ client/app/roadmaps/roadmapsFactory.js
@@ -1,6 +1,13 @@
 angular.module('roadmaps.factory', [])
 
-.factory('RoadmapsFactory', function($http){
+.factory('RoadmapsFactory', ['Server', function (Server) {
 
+  var MapFactory = {};
 
-});
+  MapFactory.cropTitle = function(title) {
+    return title.length < 12 ? title : title.slice(0, 12) + '...';
+  };
+
+  return MapFactory;
+
+}]);
